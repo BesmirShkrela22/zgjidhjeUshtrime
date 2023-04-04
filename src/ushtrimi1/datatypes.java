866c0ac972a0b5a991789f861348f50198599018
BesmirShkrela22 <besmirshkrela23@gmail.com>
2023-04-04 22:36:59 +0200

    commit message

--- src/ushtrimi1/datatypes.java
+++ src/ushtrimi1/datatypes.java
@@ -1,2 +1,20 @@
-package ushtrimi1;public class datatypes {
+package ushtrimi1;
+
+public class datatypes {
+    public static void main(String[] args) {
+int a=80;
+int b=20;
+int c=30;
+int result1=a-b-c;
+
+long d=45L;
+long e=44L;
+long f=34L;
+long result2=d*e/f;
+
+
+        System.out.println("rezultati i pare eshte "+ result1);
+        System.out.println("rezultati i dyte eshte "+ result2);
+    }
 }
+
---
+++ src/ushtrimi1/variables.java
@@ -0,0 +1,15 @@
+package ushtrimi1;
+
+public class variables {
+    public static void main(String[] args) {
+final int x=23;
+final String emri= "besmir";
+final char pseduo='b';
+final float mosha=35F;
+final boolean martuar=true;
+final double ora=12.35;
+
+
+
+    }
+}
